f59092b63144c7ea40d6113eb5647d0b5caa784d
Connor Dellwo <connor.dellwo@wsu.edu>
2022-04-24 19:45:12 -0700

    added p_value to t-test

--- src/ttest.py
+++ src/ttest.py
@@ -1,5 +1,6 @@
 # performs a t-test on two samples of cross-validation mean squared errors 
 # returns the range of the resulting p-value, which denotes the statistical significance of the difference in sample means
+import scipy.stats
 def ttest(n, mean_squared_errors_1, mean_squared_errors_2):
     # find means of mean squared error values
 
@@ -28,11 +29,11 @@ def ttest(n, mean_squared_errors_1, mean_squared_errors_2):
         i += 1
     
     t = (mu_a - mu_b) ((n(n-1) / sum_ahat_minus_bhat_sqrd) ** 1/2)
-    
+
         
 
-
-
+    p_value = scipy.stats.t.sf(abs(t), df = n-1)*2
+    
 
 
     return p_value_min, p_value_max
