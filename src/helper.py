5b078132a5d537f2d4340651cf21c2deda5bbb51
svetstufff <svetoslav.dimitrov@wsu.edu>
2022-04-24 21:23:16 -0700

    Some updates

---
+++ src/helper.py
@@ -0,0 +1,10 @@
+import mpld3
+
+def clear_last_line():
+    print ("\033[A                             \033[A")
+
+def save_graph(fig, filename):
+    html_str = mpld3.fig_to_html(fig)
+    Html_file = open(f'cross_validation_graphs/{filename}.html',"w")
+    Html_file.write(html_str)
+    Html_file.close()
