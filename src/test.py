d7762bd4295c0f2bc80143f0bff791ec49951c1e
Connor Dellwo <49624376+cdellwo@users.noreply.github.com>
2022-04-24 18:15:23 -0700

    additional cross val tests for learning rate and iterations

--- src/test.py
+++ src/test.py
@@ -12,7 +12,38 @@ alpha = 0.0052
 iterations=100
 classifiers = {
     "linear regression": LinearRegression(alpha=alpha, iterations=iterations),
-    "greedy": GreedyLinearRegression(alpha=alpha, iterations=iterations) 
+    "greedy": GreedyLinearRegression(alpha=alpha, iterations=iterations),
+    "linear regression 2.0": LinearRegression(alpha=alpha * 2, iterations=iterations),
+    "greedy 2.0": GreedyLinearRegression(alpha=alpha *2, iterations=iterations),
+    "linear regression .5" : LinearRegression(alpha = alpha / 2, iterations = iterations),
+    "greedy .5" : GreedyLinearRegression(alpha= alpha / 2, iterations = iterations),
+    "linear .25": LinearRegression(alpha = alpha / 4, iterations = iterations),
+    "greedy .25": GreedyLinearRegression(alpha = alpha / 4, iterations = iterations),
+    "linear .125": LinearRegression(alpha = alpha / 8, iterations = iterations),
+    "greedy .125": GreedyLinearRegression(alpha = alpha / 8, iterations = iterations),
+    
+    "linear regression iter = 500": LinearRegression(alpha=alpha, iterations=500),
+    "greedy iter 500": GreedyLinearRegression(alpha=alpha, iterations=500),
+    "linear regression 2.0 iter 500": LinearRegression(alpha=alpha * 2, iterations=500),
+    "greedy 2.0 iter 500": GreedyLinearRegression(alpha=alpha *2, iterations=500),
+    "linear regression .5 iter 500" : LinearRegression(alpha = alpha / 2, iterations = 500),
+    "greedy .5 iter 500" : GreedyLinearRegression(alpha= alpha / 2, iterations = 500),
+    "linear .25 iter 500": LinearRegression(alpha = alpha / 4, iterations = 500),
+    "greedy .25 iter 500": GreedyLinearRegression(alpha = alpha / 4, iterations = 500),
+    "linear .125 iter 500": LinearRegression(alpha = alpha / 8, iterations = 500),
+    "greedy .125 iter 500": GreedyLinearRegression(alpha = alpha / 8, iterations = 500),
+
+    "linear regression iter = 1000": LinearRegression(alpha=alpha, iterations=1000),
+    "greedy iter 1000": GreedyLinearRegression(alpha=alpha, iterations=1000),
+    "linear regression 2.0 iter 1000": LinearRegression(alpha=alpha * 2, iterations=1000),
+    "greedy 2.0 iter 1000": GreedyLinearRegression(alpha=alpha *2, iterations=1000),
+    "linear regression .5 iter 1000" : LinearRegression(alpha = alpha / 2, iterations = 1000),
+    "greedy .5 iter 1000" : GreedyLinearRegression(alpha= alpha / 2, iterations = 1000),
+    "linear .25 iter 1000": LinearRegression(alpha = alpha / 4, iterations = 1000),
+    "greedy .25 iter 1000": GreedyLinearRegression(alpha = alpha / 4, iterations = 1000),
+    "linear .125 iter 1000": LinearRegression(alpha = alpha / 8, iterations = 1000),
+    "greedy .125 iter 1000": GreedyLinearRegression(alpha = alpha / 8, iterations = 1000)
+
 }
 
 # plot the learning curve for simple linear regression and the Ridge classifier, which is linear regression with regulization
