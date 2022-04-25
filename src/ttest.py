0aff816b3fbb355f3d300d31d659dccdfe53f013
svetstufff <svetoslav.dimitrov@wsu.edu>
2022-04-24 18:38:49 -0700

    Defined skeleton of ttest function

---
+++ src/ttest.py
@@ -0,0 +1,12 @@
+# performs a t-test on two samples of cross-validation mean squared errors 
+# returns the range of the resulting p-value as an ordered pair, which denotes the statistical significance of the difference in sample means
+def ttest(n, mean_squared_errors_1, mean_squared_errors_2):
+    # find means of mean squared error values
+
+    # convert mean squared error values to deviations from their respective means
+
+    # use formula to compute t-value
+
+    # convert t-value to range of p-values using table
+
+    return (p_value_min, p_value_max)
